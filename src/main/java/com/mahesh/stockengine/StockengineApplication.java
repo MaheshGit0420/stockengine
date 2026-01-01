deb1c4d1f856c314d61e28a0287e698b112437e0
Mahesh Babu Guduru <Mahesh@MacBook-Pro.local>
2026-01-01 16:28:05 -0500

    repo setup

---
+++ src/main/java/com/mahesh/stockengine/StockengineApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.stockengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/stockengine/StockengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.stockengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
